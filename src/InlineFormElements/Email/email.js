a186231d74bdc871d7e5198e8117583329339953
Varun <31084854+Borumer@users.noreply.github.com>
2021-02-21 20:55:29 -0500

    Implement email change request
    v0.3.1

--- src/InlineFormElements/Email/email.js
+++ src/InlineFormElements/Email/email.js
@@ -8,8 +8,16 @@ export default function Email(props) {
 	const onEmailSaveClick = e => {
 		fetch("https://api.bforborum.com/api/login", {
 			method: "PUT",
+			headers: {
+				"content-type": "application/x-www-form-urlencoded",
+				"authorization": "Basic " + sessionStorage.getItem("apiKey")
+			},
 			body: `email=${email}`,
-		});
+		}).then(response => response.json()).then(response => {
+			if (!response.ok) {
+				alert("A system error occurred and the email could not be changed. We apologize for the inconvenience.");
+			}
+		})
 	};
 
 	return (

8179abeddd9380c0a76165ae55e2b0f66ee6761d
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-04 17:10:06 -0400

    Polish for 1.1.0 * Route forgot-password and reset-password in App
    v1.1.0

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Logout from "./Logout/logout";
 import "./App.css";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Dashboard from "./Dashboard/dashboard";
+import ForgotPassword from "./ForgotPassword/forgotPassword";
+import ResetPassword from "./ResetPassword/resetPassword";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
+				<Route exact path="/">
+					<Dashboard />
+				</Route>
 				<Route path="/signup">
 					<Register />
 				</Route>
@@ -18,8 +23,11 @@ function App() {
 				<Route path="/logout">
 					<Logout />
 				</Route>
-				<Route path="/">
-					<Dashboard />
+				<Route path="/forgot-password">
+					<ForgotPassword />
+				</Route>
+				<Route path="/reset-password">
+					<ResetPassword />
 				</Route>
 			</Switch>
 		</BrowserRouter>

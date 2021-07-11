6d0a8033ff0019ccc1065c9961bd2d51fdea783b
Varun <31084854+varunsingh87@users.noreply.github.com>
2021-07-11 18:50:10 -0400

    Change animation to slide and fix width of main container

--- src/Sidebar/sidebar.js
+++ src/Sidebar/sidebar.js
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 import Footer from "../Footer/footer";
 import sidebar from "./sidebar.module.css";
 import SidebarNav from "./sidebarNav";
-import { push as Menu } from "react-burger-menu";
+import { slide as Menu } from "react-burger-menu";
 import burgerMenu from "../styles/burgermenu.css";
 
 export default function Sidebar() {
     return window.screen.width >= 800 ? (
         <nav className={sidebar.container}>
             <SidebarNav />
-            <Link to="/logout" style={{width: '200px', padding: '1em', borderTop: '1px solid black'}}>Log Out</Link>
+            <Link to="/logout" style={{ width: '200px', padding: '1em', borderTop: '1px solid black' }}>Log Out</Link>
             <Footer />
         </nav>
     ) : (
         <Menu styles={burgerMenu} pageWrapId="page-wrap" outerContainerId="outer-container">
             <SidebarNav />
-            <Link className='menu-item' to="/logout" style={{width: '200px', padding: '1em', borderTop: '1px solid black'}}>Log Out</Link>
+            <Link className='menu-item' to="/logout" style={{ width: '200px', padding: '1em', borderTop: '1px solid black' }}>Log Out</Link>
             <Footer />
         </Menu>
     );
